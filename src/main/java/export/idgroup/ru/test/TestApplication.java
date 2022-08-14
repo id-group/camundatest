cbbedfeac026685a5a1a0a6cee0fd6b43fca8eb6
id-group <id-group@yandex.ru>
2022-08-14 10:19:17 +0300

    init

--- src/main/java/export/idgroup/ru/test/TestApplication.java
+++ src/main/java/export/idgroup/ru/test/TestApplication.java
@@ -3,9 +3,10 @@ package export.idgroup.ru.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Closeable;
+
 @SpringBootApplication
 public class TestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
